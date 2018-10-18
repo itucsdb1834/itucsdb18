d5e9bd88e500e0fa2b9e163064dce8e5bca56dc0
idilugurnal <idilugurnal@hotmail.com>
2018-10-18 13:41:40 +0300

    User class, save to db, retrieve to db is added

--- forms.py
+++ forms.py
@@ -1,20 +1,28 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+import psycopg2
 
-class RegistrationForm(FlaskForm):
-    #Validators are used to give conditions for the username , password etc.
-    name = StringField("Name" , validators=  [DataRequired()])
-    surname = StringField("Surname" , validators= [DataRequired()])
-    username = StringField("Username" , validators = [DataRequired() , Length( min = 5 , max = 15)])
-    email = StringField("Email" , validators= [DataRequired() , Email()])
-    password = PasswordField("Password" , validators = [DataRequired() , Length(min = 5 , max = 15)])
-    confirmPass = PasswordField("Confirm Password" , validators = [DataRequired() , EqualTo("password")])
-    submit = SubmitField("Register")
+class User:
+    def __init__(self, first_name , surname, username , email, password, id):
+        self.first_name = first_name
+        self.surname = surname
+        self.username = username
+        self.email = email
+        self.password = password
+        self.id = id
+    
+    def __repr__(self):
+        return "<User {}>".format(self.username)
+    
+    def save_to_db(self):
+        with psycopg2.connect(user = 'postgres' , password = '1234' , database = 'RapidEvent' , host = 'localhost') as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('INSERT INTO user_table(first_name, surname, username, email, passwrd) VALUES(%s,%s,%s,%s,%s)',
+                (self.first_name, self.surname , self.username , self.email, self.password))
 
-class LoginForm(FlaskForm):
-    email = StringField("Email" , validators= [DataRequired() , Email()])
-    password = PasswordField("Password" , validators = [DataRequired() , Length(min = 6 , max = 15)])
-    remember = BooleanField("Remember my information")
-    submit = SubmitField("Log In")
-
+    @classmethod
+    def load_from_db_with_email(cls, email):
+        cls(None, 'class' , 'classs' ,'claclas' , 'class@class.com', 1234)
+        with psycopg2.connect(user = 'postgres' , password = '1234' , database = 'RapidEvent' , host = 'localhost') as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('SELECT * FROM users WHERE email= %s' , (email,))
+                user_data = cursor.fetchone()
+                return cls(email = user_data[4] ,  first_name = user_data[1] , surname = user_data[2], username = user_data[3], password = user_data[4], id = user_data[0])
