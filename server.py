86865b1c93a25522cfda0489f158805ecd0c9829
idilugurnal <idilugurnal@hotmail.com>
2018-10-18 13:39:59 +0300

    Changes to register are done in order to link with database.

--- server.py
+++ server.py
@@ -1,48 +1,33 @@
-from flask import Flask, render_template, url_for, flash , redirect
-from forms import RegistrationForm, LoginForm
+from flask import Flask, render_template, url_for, flash , redirect 
+from forms import User
+from forms2 import LoginForm, RegistrationForm
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '0fe07ce1917042a7119c446dfa541a05'
 
-posts = [
-    {
-        'author': 'Idil Ugurnal',
-        'title': 'Big Italy Trip',
-        'content': 'Visit Italy in 7 Days',
-        'dates': '25 November 2018 - 2 December 2018'
-    },
-    {
-        'author': 'Cagla Kaya',
-        'title': 'Running Event in Besiktas',
-        'content': 'Join us in running event designed by Nike.',
-        'dates': '25 November 2018'
-    }
-]
-
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return render_template("home.html" )
+    return render_template("home.html")
 
 @app.route("/register" , methods = ['GET' , 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        my_user = User(form.name.data , form.surname.data, form.username.data , form.email.data , form.password.data, None)
+        my_user.save_to_db()
         flash(f'Your account is created with username {form.username.data}!' , 'success')
         return redirect(url_for('home_page'))
     return render_template("register.html" , title = "Register" , form = form)
 
 @app.route("/login" , methods = ['GET' , "POST"])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'idilugurnal@gmail.com'and form.password.data == '1234567' :
-            flash(f'You have logged in!' , 'success')
-            return redirect(url_for('home_page'))
-        else:
-            flash('Login unsuccessful! Check username or password.' , 'danger')
-    return render_template("login.html" , title = "Login" , form = form)
+    #form = LoginForm()
+    #if form.validate_on_submit():
+        #load_from_db_with_email()
+    pass
+
 
 @app.route("/events")
 def events_page():
