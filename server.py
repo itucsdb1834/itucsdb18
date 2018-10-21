fe440b2fcc0e31e5afac1dd62e8336e34aaf99ba
caglakaya <43502640+caglakaya@users.noreply.github.com>
2018-10-21 15:22:48 +0300

    imported database class

--- server.py
+++ server.py
@@ -1,28 +1,34 @@
-from flask import Flask, render_template, url_for, flash , redirect 
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import User
 from forms2 import LoginForm, RegistrationForm
+from dbconn import Database
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '0fe07ce1917042a7119c446dfa541a05'
+
+Database.initialise()
 
 @app.route("/")
 @app.route("/home")
 def home_page():
     return render_template("home.html")
 
-@app.route("/register" , methods = ['GET' , 'POST'])
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        my_user = User(form.name.data , form.surname.data, form.username.data , form.email.data , form.password.data, None)
+        my_user = User(form.name.data, form.surname.data, form.username.data, form.email.data, form.password.data, None)
         my_user.save_to_db()
-        flash(f'Your account is created with username {form.username.data}!' , 'success')
+        flash(f'Your account is created with username {form.username.data}!', 'success')
         return redirect(url_for('home_page'))
-    return render_template("register.html" , title = "Register" , form = form)
+    return render_template("register.html", title="Register", form=form)
 
-@app.route("/login" , methods = ['GET' , "POST"])
+
+@app.route("/login", methods = ['GET', "POST"])
 def login():
+
     #form = LoginForm()
     #if form.validate_on_submit():
         #load_from_db_with_email()
@@ -31,7 +37,9 @@ def login():
 
 @app.route("/events")
 def events_page():
-    return render_template("events.html" , posts = posts, title= 'Events')
+    return render_template("events.html", title='Events')
+
 
 if __name__ == "__main__":
+    app.debug=True
     app.run(host="0.0.0.0", port=8080, debug=True)
