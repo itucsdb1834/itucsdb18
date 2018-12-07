9b5ff169b099ab30af9cd268fa993cbf99122e7a
idilugurnal <idilugurnal@hotmail.com>
2018-12-07 22:46:56 +0300

    comment class implemented

---
+++ comment.py
@@ -0,0 +1,51 @@
+import psycopg2
+from dbconn import ConnectionPool
+import datetime
+
+class Comment():
+    def __init__(self, comment_id, owner, comment, subject, event_id, is_edited, send_notification ):
+        self.comment_id = comment_id
+        self.owner = owner
+        self.time = None
+        self.comment = comment
+        self.subject = subject
+        self.event_id = event_id
+        self.is_edited = is_edited
+        self.send_notification = send_notification
+
+    def save_to_db(self):
+        self.time = datetime.datetime.now()
+        with ConnectionPool() as cursor:
+            cursor.execute('INSERT INTO comment_table(owner,time,comment,subject,event_id,is_edited,send_notification) VALUES(%s,%s,%s,%s,%s,%s,%s)',
+             (self.owner, self.time, self.comment, self.subject, self.event_id, self.is_edited, self.send_notification))
+
+    def delete_comment(self):
+        with ConnectionPool() as cursor:
+            cursor.execute('DELETE FROM comment_table WHERE comment_id = %s' , (self.comment_id,))
+
+    def update_comment(self, comment, subject, notif):
+        self.comment = comment
+        self.subject = subject
+        self.send_notification = notif
+        self.time = datetime.datetime.now()
+        with ConnectionPool() as cursor:
+            cursor.execute('UPDATE comment_table SET comment = %s, subject = %s, send_notification = %s, is_edited = %s, time = %s' , (self.comment, self.subject, self.send_notification, True, self.time))
+
+    def get_eventid(self):
+        with ConnectionPool() as cursor:
+            cursor.execute('SELECT event_id FROM comment_table WHERE comment_id = %s' , (self.comment_id,))
+            id = cursor.fetchone()[0]
+        return id
+
+class Comments():
+    def __init__(self):
+        self.comments = []
+
+    def print_comments(self,event):
+        with ConnectionPool() as cursor:
+            cursor.execute('SELECT * FROM comment_table WHERE event_id = %s' ,(event,))
+            comments = cursor.fetchall()
+        for comment in comments:
+            temp = Comment(comment[0], comment[1], comment[3], comment[4], comment[5], comment[6], comment[7])
+            temp.time = comment[2]
+            self.comments.append(temp)
