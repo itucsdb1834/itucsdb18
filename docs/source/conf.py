e94cd4ebc8cb757e732196e95a8983b7bfb1d624
idilugurnal <idilugurnal@hotmail.com>
2018-12-26 18:40:06 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,7 +75,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinxdoc'
+html_theme = 'cakephp_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -139,7 +139,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'itucsdb18nn', 'ITUCSDB1834 Documentation',
+    (master_doc, 'itucsdb1834', 'ITUCSDB1834 Documentation',
      [author], 1)
 ]
 
@@ -150,8 +150,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'ITUCSDB1834', 'ITUCSDB18NN Documentation',
-     author, 'ITUCSDB1834', 'One line description of project.',
+    (master_doc, 'ITUCSDB1834', 'ITUCSDB1834 Documentation',
+     author, 'ITUCSDB1834', 'TravelMate',
      'Miscellaneous'),
 ]
 
